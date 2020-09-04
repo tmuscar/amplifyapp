eb181a0bc6e0e0a1145a93f3d337c204adc9848c
Tj Muscarella <tjmuscarella@gmail.com>
2020-09-04 09:41:33 -0400

    Update with test string

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hellow from V2</h1>
+        <h1> Hello from V2</h1>
+        <b>This is a test</b>
       </header>
     </div>
   );
